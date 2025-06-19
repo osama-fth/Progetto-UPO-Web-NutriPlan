315df0a2a64439c15e286a29e075499e118a1e48
osama-fth <foutihosama@gmail.com>
2025-06-19 16:48:16 +0200

    Aggiunta della modalità di apertura in scrittura per il database e implementazione di un favicon

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 const sqlite = require('sqlite3');
 const DB_NAME = process.env.DB_NAME;
 
-const db = new sqlite.Database('./' + DB_NAME, (err) => {
+const db = new sqlite.Database('./' + DB_NAME, sqlite.OPEN_READWRITE, (err) => {
   if (err) {
     console.log('Errore nella connessione: ', err.message);
   } else {
